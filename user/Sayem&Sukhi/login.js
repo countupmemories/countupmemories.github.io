0b222ef0f471840b95642edca2fac582a0a6ad21
countupmemories <countupmemories@gmail.com>
2023-09-08 07:59:50 -0700

    new user

---
+++ user/Sayem&Sukhi/login.js
@@ -0,0 +1,15 @@
+function checkLogin() {
+    const username = "sayemsukhi"; // Change this to your desired username
+    const password = "cm123@"; // Change this to your desired password
+    const usernameInput = document.getElementById("username-input").value;
+    const passwordInput = document.getElementById("password-input").value;
+    const loginIncorrect = document.getElementById("login-incorrect");
+  
+    if (usernameInput === username && passwordInput === password) {
+      document.getElementById("login-prompt").style.display = "none";
+      document.getElementById("content-container").style.display = "block";
+    } else {
+      loginIncorrect.textContent = "Username or password incorrect. Please try again.";
+    }
+  }
+  
